38558844553da23797a59060c7bc8ccb6fde875e
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-10-29 14:58:58 +0000

    Updated comments

--- script.js
+++ script.js
@@ -96,12 +96,14 @@ var finalestPassword = [];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+    // Get the length of the password and check if it is the correct length
     lengthOfPassword = prompt("How long should the password be");
     lengthOfPassword = parseInt(lengthOfPassword);
 
     if (lengthOfPassword < 8 || lengthOfPassword > 128 || isNaN(lengthOfPassword)) {
         ;
     } else {
+        // Ask the user if the password should contain lower case, upper case, numbers and special characters
         let lowerCaseLetters = confirm("Should your password contain lowercase letters");
         if (lowerCaseLetters) {
             charactersInPassword.push(...lowerCasedCharacters);
@@ -122,6 +124,7 @@ function getPasswordOptions() {
             charactersInPassword.push(...specialCharacters);
         }
 
+        // If the user selects at least one character, run the getRandom function. Else, display an alert saying the conditions were not met
         if (!(lowerCaseLetters) && !(upperCaseLetters) && !(numbers) && !(specialCharacters)) {
             alert("None of the conditions are met");
         } else {
@@ -133,7 +136,7 @@ function getPasswordOptions() {
 
 getPasswordOptions();
 
-// Function for getting a random element from an array
+// Function to get a random character for the number of characters specified by the user
 function getRandom() {
     for (let i = 0; i < lengthOfPassword; i++) {
         randomElement = charactersInPassword[Math.floor(Math.random() * charactersInPassword.length)];
@@ -142,24 +145,26 @@ function getRandom() {
     finalPassword = finalPassword.join("")
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
 document.getElementById('password').innerText = finalPassword;
 
 // Write password to the #password input
 function writePassword() {
+    // Reset the variables
     charactersInPassword = [];
     lengthOfPassword = undefined;
     randomElement = undefined;
     finalestPassword = [];
     finalPassword = [];
+    // Run the getPasswordOptions function
     getPasswordOptions();
     var passwordText = document.querySelector('#password');
     if (finalPassword.length > 8 && finalPassword.length < 128) {
         passwordText.value = finalPassword;
     }
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
